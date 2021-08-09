86f1b73a03c9deeb134614098ae6e1183a49aa84
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-09 14:39:18 -0400

    react router rendering pages

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import BlogPage from '../main/BlogPage';
+import Post from '../posts/Post';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <main>
+
+            <Switch>
+
+              <Route path="/" exact={true} render={routerProps => (
+                <BlogPage {...routerProps} />
+              )} />
+
+              <Route path="/create-post" exact={true} ender={routerProps => (
+                <Post {...routerProps} />
+              )} />
+
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    );
+  }
+
 }
+
+export default App;
