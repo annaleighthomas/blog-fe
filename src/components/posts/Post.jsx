0cba487434ab3a84aba66041c658795f7ea8a064
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-09 14:39:41 -0400

    post component

---
+++ src/components/posts/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div>
+      <input placeholder="title" />
+      <textarea placeholder="blog post"></textarea>
+    </div>
+  );
+};
+
+export default Post;
